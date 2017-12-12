46ce6ede3ce6b40fd33231e19c939ef526f4aa52
an80x86 <an80x86@gmail.com>
2017-12-12 06:46:23 +0300

    first commit

---
+++ functions/index.js
@@ -0,0 +1,13 @@
+const functions = require('firebase-functions');
+const express = require('express');
+
+const app = express();
+app.get('/timestamp', (request, response) => {
+  response.send(`${Date.now()}`);
+});
+// // Create and Deploy Your First Cloud Functions
+// // https://firebase.google.com/docs/functions/write-firebase-functions
+//
+// exports.helloWorld = functions.https.onRequest((request, response) => {
+//  response.send("Hello from Firebase!");
+// });
