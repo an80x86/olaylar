56f1e03ee453a2538bf8d0b146a3e6a3b19fab58
Mustafa Bayer <mustafa.bayer@uyumsoft.com>
2017-12-12 16:49:22 +0300

    session eklendi

--- functions/index.js
+++ functions/index.js
@@ -1,3 +1,4 @@
+const uuid = require('uuid');
 const functions = require('firebase-functions');
 const firebase = require('firebase-admin');
 const express = require('express');
@@ -87,6 +88,7 @@ app.set('views','./views');
 app.set('view engine','hbs');
 
 app.get('/', (request, response) => {
+  console.log("üretilen-no--->" + uuid.v1());
   //getLogin();
   //listAllUsers();
   response.set('Cache-Control', 'public, max-age=300, s-maxage=600');
