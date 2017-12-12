9f55a083fbd7cd8b4d34e91f5cd7f5cb9dfef6c3
Mustafa Bayer <mustafa.bayer@uyumsoft.com>
2017-12-12 10:37:36 +0300

    güncellendi

--- functions/index.js
+++ functions/index.js
@@ -8,6 +8,4 @@ app.get('/timestamp', (request, response) => {
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+exports.app = functions.https.onRequest(app);
