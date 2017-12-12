8da384bdc1b1a1669648925565e3510407c97644
Mustafa Bayer <mustafa.bayer@uyumsoft.com>
2017-12-12 11:57:14 +0300

    handlebar eklendi

--- functions/index.js
+++ functions/index.js
@@ -1,11 +1,34 @@
 const functions = require('firebase-functions');
+const firebase = require('firebase-admin');
 const express = require('express');
+const engines = require('consolidate');
+
+const firebaseApp = firebase.initializeApp(
+  functions.config().firebase
+);
+
+function getFacts() {
+  const ref = firebaseApp.database().ref('facts');
+  return ref.once('value').then(snap => snap.val());
+}
 
 const app = express();
-app.get('/timestamp', (request, response) => {
-  response.send(`${Date.now()}`);
+app.engine('hbs', engines.handlebars);
+app.set('views','./views');
+app.set('view engine','hbs');
+
+app.get('/', (request, response) => {
+  response.set('Cache-Control', 'public, max-age=300, s-maxage=600');
+  getFacts().then(facts => {
+    response.render('index', { facts });
+  });
 });
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
+
+app.get('/facts.json', (request, response) => {
+  response.set('Cache-Control', 'public, max-age=300, s-maxage=600');
+  getFacts().then(facts => {
+    response.json(facts);
+  });
+});
+
 exports.app = functions.https.onRequest(app);
