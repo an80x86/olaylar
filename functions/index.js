2a18ee215b8cedc0d24ebf92076c8ef85c8c5464
Mustafa Bayer <mustafa.bayer@uyumsoft.com>
2017-12-12 15:08:31 +0300

    login hazirliklari yapildi

--- functions/index.js
+++ functions/index.js
@@ -3,13 +3,39 @@ const firebase = require('firebase-admin');
 const express = require('express');
 const engines = require('consolidate');
 
-const firebaseApp = firebase.initializeApp(
-  functions.config().firebase
-);
+const firebaseConfig = {
+  apiKey: "AIzaSyCMIMeI5KzNVLr7zQYlaIZYudhChiW4uU0",
+  authDomain: "hesapci-6e9c0.firebaseapp.com",
+  databaseURL: "https://hesapci-6e9c0.firebaseio.com",
+  projectId: "hesapci-6e9c0",
+  storageBucket: "hesapci-6e9c0.appspot.com",
+  messagingSenderId: "353952758700"
+};
+
+var serviceAccount = require("./hesapci-6e9c0-firebase-adminsdk-nkn7c-a16b2e3215.json");
+
+const firebaseApp = firebase.initializeApp({
+    credential: firebase.credential.cert(serviceAccount),
+    databaseURL: "https://hesapci-6e9c0.firebaseio.com"
+});
 
 function getFacts() {
   const ref = firebaseApp.database().ref('facts');
   return ref.once('value').then(snap => snap.val());
+}
+
+function getLogin() {
+  var defaultAuth = firebaseApp.auth();
+  defaultAuth.getUserByEmail("an80x86@gmail.com")
+    .then(function(userRecord) {
+      console.log("Successfully fetched user data:", userRecord.toJSON());
+    })
+    .catch(function(error) {
+      console.log("Error fetching user data:", error);
+    });
+
+  // ... or use the equivalent shorthand notation
+  console.log("->>" + defaultAuth);
 }
 
 const app = express();
@@ -18,6 +44,7 @@ app.set('views','./views');
 app.set('view engine','hbs');
 
 app.get('/', (request, response) => {
+  getLogin();
   response.set('Cache-Control', 'public, max-age=300, s-maxage=600');
   getFacts().then(facts => {
     response.render('index', { facts });
